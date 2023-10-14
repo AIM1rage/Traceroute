7be92aeab13839b77b166ade641ca401963a4586
AIM1rage <michael050100@gmail.com>
2023-10-15 02:38:11 +0500

    Добавил возможность работы через консоль

--- traceroute.py
+++ traceroute.py
@@ -1,16 +1,18 @@
 import argparse
+import sys
 from scapy.all import *
 from scapy.layers.inet import IP, ICMP
 
 
 class Traceroute:
     MIN_SIZE = 28
+    MAX_SIZE = 65535
 
     def __init__(self, host, timeout=1, delay=0, max_ttl=30, count=3, size=40):
         self.host = socket.gethostbyname(host)
 
         self.max_hops = max_ttl
-        self.repeats = count
+        self.count = count
 
         self.timeout = timeout
         self.delay = delay
@@ -24,7 +26,7 @@ class Traceroute:
             src = None
             pings = []
             icmp_type = None
-            for _ in range(self.repeats):
+            for _ in range(self.count):
                 start = time.time()
                 reply = self.get_ipv4_reply(self.get_ipv4_packet(ttl))
                 if not reply[0]:
@@ -59,5 +61,41 @@ class Traceroute:
 
 
 if __name__ == '__main__':
-    traceroute = Traceroute('habrahabr.ru', 2, 0, size=28)
+    parser = argparse.ArgumentParser(prog='traceroute',
+                                     description='Traceroute traces network packet paths and identifies intermediate routers and their timings.')
+    parser.add_argument('host',
+                        nargs=1,
+                        type=str,
+                        help='host name or ip-address')
+    parser.add_argument('-ttl',
+                        nargs=1,
+                        default=30,
+                        type=int,
+                        help='maximum time-to-live value')
+    parser.add_argument('-c',
+                        nargs=1,
+                        default=3,
+                        type=int,
+                        help='requests count')
+    parser.add_argument('-d',
+                        nargs=1,
+                        default=0,
+                        type=int,
+                        help='delay between requests in seconds')
+    parser.add_argument('-t',
+                        nargs=1,
+                        default=1,
+                        type=int,
+                        help='request timeout in seconds')
+    parser.add_argument('-s',
+                        nargs=1,
+                        default=40,
+                        type=int,
+                        help='packet size')
+    args = parser.parse_args()
+    traceroute = Traceroute(host=args.host[0],
+                            timeout=args.t,
+                            delay=args.d,
+                            count=args.c,
+                            size=args.s)
     traceroute.trace()
