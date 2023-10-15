776bf486c775e38ce3888083c1f261cb8ff8cabb
AIM1rage <michael050100@gmail.com>
2023-10-16 03:18:03 +0500

    Рефакторинг. Добавил число попыток отправки посылки

--- traceroute.py
+++ traceroute.py
@@ -8,7 +8,7 @@ class Traceroute:
     MAX_SIZE = 65535
 
     def __init__(self, host,
-                 seq=0, timeout=1, delay=0.5, max_ttl=30, count=3, size=40):
+                 seq=0, timeout=1, delay=0, max_ttl=30, count=3, size=40):
         self.host = socket.gethostbyname(host)
 
         self.seq = seq
@@ -38,24 +38,27 @@ class Traceroute:
             for _ in range(self.count):
                 start = time.time()
                 reply = self.get_ipv4_reply(self.get_ipv4_packet(ttl))
-                if not reply[0]:
+                if reply is None:
                     pings.append(None)
                     continue
-                answer = reply[0][0].answer
-                end = answer.time
-                src = answer.src
-                icmp_type = answer[ICMP].type
+                end = reply.time
+                src = reply.src
+                icmp_type = reply[ICMP].type
                 pings.append(round((end - start) * 1000))
-                time.sleep(self.delay)
             yield ttl, pings, src
             if icmp_type == 0:
                 break
 
     def get_ipv4_packet(self, ttl):
-        return IP(dst=self.host, ttl=ttl, len=self.size) / ICMP(seq=self.seq)
+        return IP(dst=self.host, ttl=ttl, len=self.size) / ICMP(type=8,
+                                                                seq=self.seq)
 
     def get_ipv4_reply(self, ipv4_packet):
-        return sr(ipv4_packet, timeout=self.timeout, verbose=False)
+        return sr1(ipv4_packet,
+                   timeout=self.timeout,
+                   inter=self.delay,
+                   verbose=False,
+                   retry=-3)
 
     @staticmethod
     def print_row(ttl, pings, src):
@@ -80,7 +83,7 @@ if __name__ == '__main__':
                         help='maximum time-to-live value')
     parser.add_argument('-c', default=3, type=int,
                         help='requests count')
-    parser.add_argument('-d', default=0.5, type=float,
+    parser.add_argument('-d', default=0, type=float,
                         help='delay between requests in seconds')
     parser.add_argument('-t', default=1, type=int,
                         help='request timeout in seconds')
